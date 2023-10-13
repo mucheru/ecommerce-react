bdbc9303f942a12f18d7ee1f2d23cc1bae9701e7
steve <stephen@stephenmucheru79@gmail.com>
2023-10-13 15:20:28 +0300

    updated app file and components file

---
+++ src/components/ShowCourseComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ShowCourseComponent({courses,filterCourseFunction,addCourseToCartFunction}){
+
+    return (
+        <div className="product-list">
+            {filterCourseFunction.length === 0 ?(
+                <p className="no-results">Sorry no matching product found</p>
+            ):(
+                filterCourseFunction.map((product)=>(
+                    <div className="product" key={product.id}>
+                        <img  src={product.image} alt={product.name}/>
+                        <h2>{product.name}</h2>
+                        <p>Price:/=ksh {product.price}</p>
+                        <button className="add-to-cart-button" onClick = {()=>addCourseToCartFunction(product)}>
+                            Add to Shopping cart
+
+                        </button>
+                    </div>
+                ))
+            )}
+            
+        </div>
+    );
+
+}
+export default ShowCourseComponent;
